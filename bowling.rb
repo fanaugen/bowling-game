0dd45d82dda3600c4218582aef7bb97c3a1efffc
Priya Patil <priya10487@aol.com>
2014-11-26 20:55:08 +0000

    Add all up
    Adds up all the pins

--- bowling.rb
+++ bowling.rb
@@ -1,47 +1,30 @@
-# class Frame
-#   def initialize
-#     roll
-#   end 
-
-# end
-
 class BowlingGame
 	
-  attr_accessor :frames
-
   def initialize
-    self.frames = []
+    @frames = []
   end
 
-
   def roll(pins)
-    self.frames << pins
-    puts pins 
+    @frames.push pins
 	end
 
   def score
-    total_score = 0
-    self.frames.each_with_index do |frame, i|
-      total_score += frame[0] + frame[1].to_i
-
-      if frames[(i-1)] == [10] or frames[(i - 2)] == [10]
-        total_score += frame[0] + frame[1].to_i
-      end
-    end
-    
-    total_score
+    @frames.reduce &:+
+    #### equivalent to:
+    # total = 0
+    # @frames.each do |frame|
+    #   total += frame
+    # end
+    # total
   end
 
   def print_status
-    print "Still playing!"
 
   end
 
 end
 
-
-
-
+###########################################################
 pins_knocked_down = [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6]
 expected_total_score = 133
 
